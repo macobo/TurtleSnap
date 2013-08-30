cefa9530ad5dcb7bfa0b669801e2efb434d7391b
Karl-Aksel Puulmann <macobo@ut.ee>
2013-08-30 21:08:12 +0300

    More robust gif creation

--- creategif.sh
+++ creategif.sh
@@ -3,15 +3,9 @@
 FILE_PREFIX=$1
 OUT_FILE_NAME=$2
 FRAME_DELAY=$3
-TEMP_FILE="_animation.gif"
 
-echo $TEMP_FILE
-convert -delay $FRAME_DELAY -dispose previous "$FILE_PREFIX???.ps" $TEMP_FILE
-echo $TEMP_FILE
-CROPSIZE=`convert $TEMP_FILE -layers merge -trim -format '%wx%h%X%Y' info:`
-echo $TEMP_FILE
-convert $TEMP_FILE -crop $CROPSIZE! $OUT_FILE_NAME
-echo $TEMP_FILE
+CROPSIZE=`convert $FILE_PREFIX???.ps  -layers flatten -trim -format '%wx%h%X%Y' info:`
+convert -delay 20 -dispose previous $FILE_PREFIX???.ps \
+        -crop $CROPSIZE! +repage $OUT_FILE_NAME
 
-rm $TEMP_FILE
 rm $FILE_PREFIX*
