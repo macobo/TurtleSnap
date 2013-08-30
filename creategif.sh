e43e260c52ce76016f9ecdd876682eb5aa51f21c
Karl-Aksel Puulmann <macobo@ut.ee>
2013-08-30 22:16:04 +0300

    Antialiasing

--- creategif.sh
+++ creategif.sh
@@ -1,11 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-FILE_PREFIX=$1
+FILE_PREFIX=$1???.ps
 OUT_FILE_NAME=$2
 FRAME_DELAY=$3
+DPI=90
 
-CROPSIZE=`convert $FILE_PREFIX???.ps  -layers flatten -trim -format '%wx%h%X%Y' info:`
-convert -delay 20 -dispose previous $FILE_PREFIX???.ps \
-        -crop $CROPSIZE! +repage $OUT_FILE_NAME
+CROPSIZE=`convert -density $DPI $FILE_PREFIX -layers flatten -trim -format '%wx%h%X%Y' info:`
+
+convert -delay 20 \
+        -dispose previous \
+        -density $DPI \
+        -quality 100 \
+        +antialias \
+        $FILE_PREFIX \
+        -crop $CROPSIZE! \
+        +repage \
+        $OUT_FILE_NAME
 
 rm $FILE_PREFIX*
